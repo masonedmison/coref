468c8b9fe2a9a05825eda55b9e1c86920002f576
Mason Edmison <mzbaran@uwm.edu>
2019-10-31 11:49:42 -0500

    eval skeleton

--- bionlp_eval.py
+++ bionlp_eval.py
@@ -1,3 +1,119 @@
 """A script that evaluates coference resolution on BioNLP 2011 training data"""
 
+"""
+regarding matches:
+    (1) Exact match:
+        - begin(detected mention)= begin(gold mention) & end(detected mention)= end(gold mention)
+    (2) Partial match based on minimal and maximal boundaries of gold mentions:
+        - begin(detected mention)>=begin(maximal boundary) & end(detected mention)<=end(maximal boundary)
+        - begin(detected mention)<=begin(minimal boundary) & end(detected mention)>=end(minimal boundary)   
+"""
+# --------------------------------------------------
+# PSUEDO CODE 
 
+# Assuming out of of doc._coref_clusters...
+
+def commutative_pairing(cluster_list, strip_spacy_attrs=True):
+    """ Takes a list of clusters and pairs them off commutatively.
+    e.g. [t1,t2,t3,t4] --> (t1,t2), (t1,t3), (t1,t4).
+    This function expects a coref_clusters object (a list of clusters) that has been resolved by spacy
+    Args:
+        cluster_list: a list of cluster resolved by Spacy
+        strip_spacy_attrs: if True, strip spacy objects to text word representation only
+    Returns:
+        list of tuples with len == 2 where members are SpaCY span objects or text depending on strip_spacy_attrs. 
+    """
+    pass
+
+
+# convert word index to char index - SANITY CHECK
+def word_to_char_indices(words_pair, container_text):
+    """Extracts character index spans for a pair of words
+    Current implementation only accepts type(word[0]) == str. 
+    This function also drops the string value - keep this in mind for future development
+    Args: 
+        words_pair: word or phrase that to be convereted
+        container_text: text where the word or phrase is contained in
+    Returns:
+        tuple with each word as a character index span
+    """
+    pass
+
+# get corresponding a2 file 
+def get_true_mentions(f_path):
+    """ Takes full path to file of current abstract that is being processed and gets a2 file
+    Args:
+        f_path: Path to current file being processed
+    Returns:
+        str - Corresponding *.a2 
+    """
+    pass
+    
+# slice relevant section from line containting beginning with R1 to end of doc
+def get_gold_coref_exps(a2_f_path):
+    """extracts relevant area of .a2 txt document containing coreferring expressions
+    Args:
+        a2_f_path: file path to a2 file 
+    Returns:
+        section of document containing coref expressions
+    """
+    pass
+
+# extract spans of expressions for each coref exp
+def get_coref_spans(coref_exp):
+    """Get pair clusters containing character indices from a coref_exp (BART format)
+    Args:
+        coref_exp: a coreference expression, e.g. 'R3	Coref Anaphora:T19 Antecedent:T15'
+    Returns:
+        tuple of len == 2 where each entry is a character index span
+    """
+    pass
+# where AD = Annotated Dataset and NCP= Neural Coref Predicitions
+
+# AD = True ---- NCP= Predicted
+# when comparing predicited vs true 
+# ----> have a function to detect atom links, ie Surface ->(T3,T2), (T2, T1) == Atom-> (T3,T1)
+
+# ----------------------------------------------------
+# get stuff to calculate metrics (prec, recall, f1)
+
+# for every prediction that is right = True Positive
+
+# for every incorrect predicition = False Positive 
+
+# for each prediction we did not get = False Negative 
+
+# ----------------------------------------------------
+
+# ----------------------------------------------------
+# metrics
+def precision(true_pos, false_pos):
+    """Calculates Precision metric scocre
+    Args: 
+        true_pos: Correct predictions according Gold
+        false_pos: predicted positives that are no in Gold
+    Returns:
+        precision metrics score
+    """
+    return true_pos/(true_pos+false_pos)
+
+def recall(true_pos, false_neg):
+    """computes recall metric score
+    Args:
+        true_pos: Correct predicitions according to Gold
+        false_neg: missed predictions according to Gold
+    Returns:
+        recall metric score
+    """
+    return true_pos/(true_pos+false_neg)
+
+def f1(prec, rec):
+    """ Calculate F1 score or Harmonic Mean
+    Args:
+        prec: precision score
+        rec: recall score
+    Returns:
+        f1 metric score
+    """
+    return 2 * (prec*rec)/(prec+rec)
+# ----------------------------------------------------
--- explorations.py
+++ explorations.py
@@ -8,44 +8,28 @@ neuralcoref.add_to_pipe(nlp, greedyness=0.5)
 
 """playground for nueral coref"""
 
-toy_sent = 'My sister has a dog. She loves him'
+toy_sent = 'My sister has a furry-dog. She loves him. Her favorite color is blue. She likes this because it is pretty. The dog is pretty good overall.'
 
-doc = nlp(pub_ab)
+doc = nlp(toy_sent)
 
-print(f'[TOY SENTENCE] {pub_ab}')
-print('-'*35)
-
-print('[Coref Cluster things]')
-print('all clusters in doc', doc._.coref_clusters)
-# print(f'scores {doc._.coref_scores}')
-# print(f'ALL mentions in first clusters {doc._.coref_clusters[1].mentions}')
-# print(f'last mention in first cluster {doc._.coref_clusters[1].mentions[-1]}')
-# print(f'?? {doc._.coref_clusters[1].mentions[-1]._.coref_cluster.main}')
-
-print('-'*35)
-
+print(doc._.coref_clusters)
 
 
 # a peak at spans and their indices
-for s in doc._.coref_clusters:
-    print(f'[CLUSTER] {s}')
-    for ss in s.mentions:
-        print(f'[MENTION] {ss}')
+for i,s in enumerate(doc._.coref_clusters):
+    # print(f'[CLUSTER] {s}')
+    print(f'antecedent {doc._.coref_clusters[i].mentions[0]}')
+    for i,ss in enumerate(s.mentions):
+        if i == 0:
+            continue
+        print(f'[Anaphora] {ss}')
         print('start of span' ,ss.start)
         print('end of span', ss.end)
         print('-'*35)
 
-# print('[Tokens things]')
-# token = doc[-1]
-# print(f'is token "{token}" in coref clusters {token._.in_coref}')
-# print(f'clusters that contain THIS token {token._.coref_clusters}')
-#
-# print('-'*35)
-#
-# print('[Span things]')
-# span = doc[-1:]
-# print(f'is span "{span}" in at least one coref mention {span._.is_coref}')
-# print(f'Span of the most representative mention in the cluster {span._.coref_cluster.main}')
-# print(f'cluster of most representative mention {span._.coref_cluster.main._.coref_cluster}')
-#
-# print('-'*35)
+
+word_ = 'furry-dog'
+# get char i of furry-dog at (using str.index) -- span to len(furry-dog)
+d_index = toy_sent.index(word_)
+d_char_span = toy_sent[d_index:d_index+len(word_)]
+print(d_char_span)
